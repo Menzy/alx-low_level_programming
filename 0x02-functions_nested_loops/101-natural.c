69f6f4dc62dcf63d074fa31ad121a6ae2233b4af
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 22:44:51 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Info - Prints the sum of all multiples of 3 or 5 up to 102
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a;
+		}
+
+		a++;
+	}
+
+	printf("%d\n", b);
+	return (0);
+}
