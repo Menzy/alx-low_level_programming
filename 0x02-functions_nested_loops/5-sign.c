b538df083268cff72781fe12f430b4ccffaab2eb
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 21:38:55 -0800

    thirteenth

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_sign - print if the sign of a number is + or -
  * @n: number to be checked
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int add(int a, int b);
 void times_table(void);
 void print_to_98(int n);
 void jack_bauer(void);
+void print_times_table(int n);
