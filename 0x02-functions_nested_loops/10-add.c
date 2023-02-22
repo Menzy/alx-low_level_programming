488666ed0dc1c08d7c32d75b63eda5c95d3e4c3b
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 18:40:14 -0800

    eleventh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - func to add two integers
+ * @a: first int
+ * @b: second int
+ * Return: results of the addition
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	
+	return (sum);
+}
