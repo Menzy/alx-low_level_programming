9dd770f54a4bcb7e9ecc8c09a38d2b608941a183
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 21:46:11 -0800

    header update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int add(int a, int b);
 void times_table(void);
 void print_to_98(int n);
 void jack_bauer(void);
-void print_times_table(int n);
+void print_times_table(int n):
+int print_sign(int n);
