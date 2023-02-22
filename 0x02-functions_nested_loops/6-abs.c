9e8270b20ad5042df7d5d03ae183fb4e2d00c773
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 18:19:24 -0800

    eight

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,1 @@
-#include "holberton.h"
 
-/**
- * print_last_digit - prints the last digit of a number
- * @n: n is an integer
- * Description: prints the last digit of a number
- * Return: value of the last digit.
- */
-int print_last_digit(int i)
-{
-	int last = i % 10;
-
-	if (i < 0)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
-}
