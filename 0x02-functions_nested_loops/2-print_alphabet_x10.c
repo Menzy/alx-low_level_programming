a187eca1234251aa71248ee723705b9ab14200e4
Joshua Mensah <joshuamensah46@gmail.com>
2023-02-21 17:34:29 -0800

    third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Prints 10 times the alphabet, in lowercase.
+ * Return. Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+
+}
+
