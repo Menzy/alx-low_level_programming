c1570e6c7e3dedd49d44af0f3243c81f0d7746e1
Joshua Mensah <joshuamensah46@gmail.com>
2023-03-14 22:57:15 -0700

    two strings

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
@@ -9,35 +7,38 @@
  * @height: array height
  *Return: pointer to a 2-dimensional array
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **ptr;
-	int a, b;
+	int **D;
+	int height_index, width_index;
 
-	if ((width <= 0) || (height <= 0))
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int **)malloc(width * sizeof(int *));
-	for (a = 0; a < width; a++)
-		ptr[a] = (int *)malloc(height * sizeof(int));
-	if (ptr == NULL)
-	{
-		printf("Memory allocation failed\n");
-		for (a = 0; a < width; a++)
-		{
-			free(ptr[a]);
-		}
-		free(ptr);
-	}
-	else
-	{
-		for (a = 0; a < height; a++)
-		{
-			for (b = 0; b < width; b++)
-				ptr[a][b] = 0;
-		}
+	D = malloc(sizeof(int *) * height);
 
+	if (D == NULL)
+		return (NULL);
+
+	for (height_index = 0; height_index < height; height_index++)
+	{
+		D[height_index] = malloc(sizeof(int) * width);
+
+		if (D[height_index] == NULL)
+		{
+			for (; height_index >= 0; height_index--)
+				free(D[height_index]);
+
+			free(D);
+			return (NULL);
+		}
 	}
-	return (ptr);
+
+	for (height_index = 0; height_index < height; height_index++)
+	{
+		for (width_index = 0; width_index < width; width_index++)
+			D[height_index][width_index] = 0;
+	}
+
+	return (D);
 }
