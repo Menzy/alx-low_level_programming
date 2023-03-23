64ad67a38eaf8c9edfae7780a9122dd8c7f58f6d
Joshua Mensah <joshuamensah46@gmail.com>
2023-03-22 20:57:10 -0700

    completed tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
-
 /**
  * print_name - prints a name
  * @name: string input
  * @f: pointer to a function
  * Return:
  */
- void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
