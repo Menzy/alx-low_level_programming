e25ff77d015cdd993c12c01cee5106926a888960
Joshua Mensah <joshuamensah46@gmail.com>
2023-03-31 22:35:27 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,23 +13,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
+	list_t *new;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (NULL);
-    }
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
-    new->len = strlen(str);
-    new->next = *head;
-    *head = new;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
