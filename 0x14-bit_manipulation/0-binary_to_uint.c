a9788668ee255206c136b53dafd12b00671cb488
Joshua Mensah <joshuamensah46@gmail.com>
2023-04-10 13:17:29 -0700

    Task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1
- * Return: the converted number, or 0 if b is NULL or contains non-binary characters
+ * Return: the converted number, or 0 if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -11,16 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return 0;
+			return (0);
 
 		result <<= 1;
 		result += b[i] - '0';
 	}
 
-	return result;
+	return (result);
 }
