f681e827ebf314554cecfb26e76b210ac65b2a6d
Menzy <joshuamensah46@gmail.com>
2024-01-25 20:57:55 +0000

    feat: Add main.h header file
    The main.h header file defines the structure for a doubly linked list node and declares functions for various operations on a doubly linked list.

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head node receiving argument
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t counter = 0;
+
+    // Loop through the list until the end (NULL) is reached
+    while (h != NULL)
+    {
+        // Print the data of the current node
+        printf("%d\n", h->n);
+
+        // Move to the next node
+        h = h->next;
+
+        // Increment the counter
+        counter++;
+    }
+
+    // Return the number of nodes in the list
+    return (counter);
+}
