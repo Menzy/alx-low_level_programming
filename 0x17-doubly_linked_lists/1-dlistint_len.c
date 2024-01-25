d7123ed96766a14281064864a9a41d30727344c3
Menzy <joshuamensah46@gmail.com>
2024-01-25 21:31:53 +0000

    feat: Implement dlistint_len function
    The dlistint_len function calculates the number of elements in a doubly linked list, providing a quick way to determine the length of the list.

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,18 +7,18 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    // Loop through the list until the end (NULL) is reached
-    while (h != NULL)
-    {
-        // Move to the next node
-        h = h->next;
+	/* Loop through the list until the end (NULL) is reached */
+	while (h != NULL)
+	{
+		/* Move to the next node */
+		h = h->next;
 
-        // Increment the counter
-        counter++;
-    }
+		/* Increment the counter */
+		counter++;
+	}
 
-    // Return the number of nodes in the list
-    return (counter);
+	/* Return the number of nodes in the list */
+	return (counter);
 }
