aab3f7e03a32ff77f22bd049d332612f22144d51
Menzy <joshuamensah46@gmail.com>
2024-01-25 21:32:10 +0000

    feat: Implement add_dnodeint function
    The add_dnodeint function adds a new node at the beginning of a doubly linked list, helping in efficiently inserting elements at the front.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,33 +8,33 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
+	dlistint_t *new;
 
-    // Allocate memory for the new node
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        // Print an error message if memory allocation fails
-        printf("Error\n");
-        return (NULL);
-    }
+	/* Allocate memory for the new node */
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		/* Print an error message if memory allocation fails */
+		printf("Error\n");
+		return (NULL);
+	}
 
-    // Set the data of the new node
-    new->n = n;
+	/* Set the data of the new node */
+	new->n = n;
 
-    // Set the next pointer of the new node to the current head
-    new->next = *head;
+	/* Set the next pointer of the new node to the current head */
+	new->next = *head;
 
-    // Set the previous pointer of the new node to NULL (since it's added at the beginning)
-    new->prev = NULL;
+	/* Set the previous pointer of the new node to NULL (since it's added at the beginning) */
+	new->prev = NULL;
 
-    // If the list is not empty, set the previous pointer of the current head to the new node
-    if (*head != NULL)
-        (*head)->prev = new;
+	/* If the list is not empty, set the previous pointer of the current head to the new node */
+	if (*head != NULL)
+		(*head)->prev = new;
 
-    // Update the head to point to the new node
-    *head = new;
+	/* Update the head to point to the new node */
+	*head = new;
 
-    // Return the address of the new node
-    return (new);
+	/* Return the address of the new node */
+	return (new);
 }
