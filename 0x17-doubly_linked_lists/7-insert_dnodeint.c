934cad7b3f3ca5eb5efe377dfe0b930a19e809c5
Menzy <joshuamensah46@gmail.com>
2024-01-26 05:38:41 +0000

    fixed errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,40 +12,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp1, *temp2 = NULL, *new;
 	unsigned int i = 0;
 
-	/* Create a new node */
-	new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t)); /* Create a new node */
 	if (new == NULL)
 		return (NULL);
-
-	/* Insert n value into the new node */
-	new->n = n;
-
-	/* If the list is empty, insert at the beginning */
-	if (*h == NULL)
+	new->n = n;	/* Insert n value into the new node */
+	if (*h == NULL) 	/* If the list is empty, insert at the beginning */
 	{
 		*h = new;
 		return (new);
 	}
-
-	/* If index is 0, insert at the beginning */
-	if (idx == 0)
+	if (idx == 0) /* If index is 0, insert at the beginning */
 	{
 		new->next = *h;
 		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
+	temp1 = temp2 = *h; /* Initialize temporary pointer to traverse list */
 
-	/* Initialize temporary pointers to traverse the list */
-	temp1 = temp2 = *h;
-
-	/* Traverse the list */
-	while (temp1 != NULL)
+	while (temp1 != NULL) /* Traverse the list */
 	{
 		if (i == idx)
 		{
-			/* Insert the new node into the list */
-			new->next = temp1;
+			new->next = temp1; /* Insert the new node into the list */
 			new->prev = temp2;
 			temp2->next = temp1->prev = new;
 			return (new);
@@ -54,10 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp1 = temp1->next;
 		i++;
 	}
-
 	/* If the index is out of range, free the new node and return NULL */
 	if (i < idx)
 		free(new);
-
 	return (NULL);
 }
