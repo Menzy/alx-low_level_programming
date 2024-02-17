a1b832650a1c1f221cce2e1b8cf1e61a2fcfa6c5
Menzy <joshuamensah46@gmail.com>
2024-02-17 00:48:10 +0000

    new fixes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,39 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes the hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - Deletes a hash table.
+ * @ht: Pointer to the hash table to be deleted.
+ *
+ * This function frees the memory allocated for the hash table @ht,
+ * including all its elements.
+ *
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int c;
+    hash_node_t *cur_hashnode;
+    unsigned long int i = 0;
 
-	for (c = 0; c < ht->size; c++)
-	{
-		if (ht->array[c] != NULL)
-		{
-			node = ht->array[c];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
-		}
-	}
-	free(head->array);
-	free(head);
+    /* Return if the hash table is NULL */
+    if (ht == NULL)
+        return;
+
+    /* Iterate through each bucket in the hash table */
+    while (i < ht->size)
+    {
+        /* Free memory for each node in the linked list */
+        while (ht->array[i] != NULL)
+        {
+            cur_hashnode = ht->array[i];
+            free(cur_hashnode->key);
+            free(cur_hashnode->value);
+            ht->array[i] = ht->array[i]->next;
+            free(cur_hashnode);
+        }
+        i++;
+    }
+
+    /* Free the array of pointers and the hash table structure itself */
+    free(ht->array);
+    free(ht);
 }
